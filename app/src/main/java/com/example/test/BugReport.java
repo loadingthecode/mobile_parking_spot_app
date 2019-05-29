c3690fd3f442155494e65fd3dac060702487319a
beard <oluo@rollins.edu>
2019-05-29 12:25:01 -0400

    finished bug report activity

--- app/src/main/java/com/example/test/BugReport.java
+++ app/src/main/java/com/example/test/BugReport.java
@@ -1,9 +1,16 @@
 package com.example.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BugReport extends AppCompatActivity {
+    //private EditText to;
+    private EditText subject;
+    private EditText bugReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,38 @@ public class BugReport extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bug_report);
+
+        //to = findViewById(R.id.to);
+        subject = findViewById(R.id.userSubject);
+        bugReport = findViewById(R.id.userBugReport);
+
+        Button bugReport = findViewById(R.id.sendBugReport);
+
+        bugReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMail();
+            }
+        });
     }
-}
+
+        public void sendMail() {
+            //String recipientList = to.getText().toString();
+            //String[] recipients = recipientList.split(",");
+
+            String[] recipients = new String[]{"parkingappreports@gmail.com"};
+
+            //recipients[0] = "parkingappreports@gmail.com";
+
+            String mailSubject = subject.getText().toString();
+            String message = bugReport.getText().toString();
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+            intent.putExtra(Intent.EXTRA_SUBJECT, mailSubject);
+            intent.putExtra(Intent.EXTRA_TEXT, message);
+
+            intent.setType("message/rfc822");
+            startActivity(Intent.createChooser(intent, "Choose an email client"));
+        }
+    }
--- app/src/main/java/com/example/test/Help.java
+++ app/src/main/java/com/example/test/Help.java
@@ -1,7 +1,11 @@
 package com.example.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Help extends AppCompatActivity {
 
@@ -11,5 +15,6 @@ public class Help extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // adds back arrow
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
     }
 }
