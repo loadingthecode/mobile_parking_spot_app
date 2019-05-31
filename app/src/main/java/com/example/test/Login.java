c1042e9a26bca7e2b1f5cc4550612717c867a793
beard <oluo@rollins.edu>
2019-05-31 10:24:13 -0400

    latest

--- app/src/main/java/com/example/test/Login.java
+++ app/src/main/java/com/example/test/Login.java
@@ -2,12 +2,9 @@ package com.example.test;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
-import android.util.Log;
-import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -23,6 +20,4 @@ public class Login extends AppCompatActivity {
 
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFD700")));
     }
-
-
 }
