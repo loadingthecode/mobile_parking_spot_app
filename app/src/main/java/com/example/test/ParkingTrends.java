a22eb0c247671fb08531d8c64ad4a94e6df3c004
beard <oluo@rollins.edu>
2019-06-04 15:35:41 -0400

    fixed spacing issue on trends chart. also added more colors to bars.

--- app/src/main/java/com/example/test/ParkingTrends.java
+++ app/src/main/java/com/example/test/ParkingTrends.java
@@ -6,10 +6,15 @@ import android.widget.ArrayAdapter;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
+import com.github.mikephil.charting.components.AxisBase;
+
 
 import java.util.ArrayList;
 
@@ -26,12 +31,22 @@ public class ParkingTrends extends AppCompatActivity {
         setContentView(R.layout.activity_parking_trends);
 
         trendsChart = (BarChart) findViewById(R.id.trendsGraph);
+
+        trendsChart.setDrawBarShadow(false);
+        trendsChart.setDrawValueAboveBar(true);
+        trendsChart.setMaxVisibleValueCount(50);
+        trendsChart.setPinchZoom(false);
+        trendsChart.setDrawGridBackground(true);
+
         ArrayList<BarEntry> barEntries = new ArrayList<>();
+
         barEntries.add(new BarEntry(30f, 0));
         barEntries.add(new BarEntry(10f, 1));
         barEntries.add(new BarEntry(26f, 2));
         barEntries.add(new BarEntry(3f, 3));
+
         BarDataSet barDataSet = new BarDataSet(barEntries, "Time of Day");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS); // from barchart video indian speaker
 
         ArrayList<String> theTimes = new ArrayList<String>();
         theTimes.add("Before 9AM");
@@ -40,18 +55,16 @@ public class ParkingTrends extends AppCompatActivity {
         theTimes.add("3PM-5PM");
 
         BarData numberOfParkings = new BarData(theTimes, barDataSet);
+
+        trendsChart.getXAxis().setSpaceBetweenLabels(2); // edit the space between labels
+
         trendsChart.setData(numberOfParkings);
 
         trendsChart.setTouchEnabled(true);
         trendsChart.setDragEnabled(true);
         trendsChart.setScaleEnabled(true);
 
-        trendsChart.setMaxVisibleValueCount(4);
-
         barDataSet.setBarSpacePercent(75f);
-
-
-        // TODO: read documentation to make bars thinner
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, dayList);
