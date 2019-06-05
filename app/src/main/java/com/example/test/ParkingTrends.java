159a96165401012cf281aab1588726a9b6d0b0c8
beard <oluo@rollins.edu>
2019-06-05 13:17:16 -0400

    added basic backend functionality to trends. you can now view day-specific parking trends.

--- app/src/main/java/com/example/test/ParkingTrends.java
+++ app/src/main/java/com/example/test/ParkingTrends.java
@@ -1,11 +1,19 @@
 package com.example.test;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -21,17 +29,34 @@ import com.github.mikephil.charting.components.AxisBase;
 
 import java.util.ArrayList;
 
-public class ParkingTrends extends AppCompatActivity {
+public class ParkingTrends extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     BarChart trendsChart;
     String [] dayList={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    ArrayList<BarEntry> barEntries = new ArrayList<>();
+    BarDataSet barDataSet = new BarDataSet(barEntries, "Time of Day");
+
+    BarEntry earlyMorning;
+    BarEntry lateMorning;
+    BarEntry earlyAfternoon;
+    BarEntry lateAfternoon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().setTitle("Parking Trends"); // set actionbar title
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // adds back arrow
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_trends);
+
+        ActionBar bar = getSupportActionBar();
+        bar.setTitle("Parking Trends"); // set actionbar title
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFD700")));
+
+        Spinner spinner = findViewById(R.id.trendsDayChooser);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.dayOfWeek, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
 
         trendsChart = (BarChart) findViewById(R.id.trendsGraph);
 
@@ -41,14 +66,16 @@ public class ParkingTrends extends AppCompatActivity {
         trendsChart.setPinchZoom(false);
         trendsChart.setDrawGridBackground(true);
 
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        earlyMorning = new BarEntry(30f, 0);
+        lateMorning = new BarEntry(10f, 1);
+        earlyAfternoon = new BarEntry(26f, 2);
+        lateAfternoon = new BarEntry(3f, 3);
 
-        barEntries.add(new BarEntry(30f, 0));
-        barEntries.add(new BarEntry(10f, 1));
-        barEntries.add(new BarEntry(26f, 2));
-        barEntries.add(new BarEntry(3f, 3));
+        barEntries.add(earlyMorning);
+        barEntries.add(lateMorning);
+        barEntries.add(earlyAfternoon);
+        barEntries.add(lateAfternoon);
 
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Time of Day");
         barDataSet.setColors(ColorTemplate.COLORFUL_COLORS); // from barchart video indian speaker
 
         ArrayList<String> theTimes = new ArrayList<String>();
@@ -73,10 +100,45 @@ public class ParkingTrends extends AppCompatActivity {
         trendsChart.setScaleEnabled(true);
 
         barDataSet.setBarSpacePercent(75f);
+    }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, dayList);
-        MaterialBetterSpinner daySpinner = (MaterialBetterSpinner)findViewById(R.id.trendsDayChooser);
-        daySpinner.setAdapter(arrayAdapter);
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+
+        if (text.equals("Monday")) {
+            earlyMorning.setVal(5f);
+            lateMorning.setVal(20f);
+            earlyAfternoon.setVal(30f);
+            lateAfternoon.setVal(12f);
+        } else if (text.equals("Tuesday")) {
+            earlyMorning.setVal(12f);
+            lateMorning.setVal(23f);
+            earlyAfternoon.setVal(25f);
+            lateAfternoon.setVal(19f);
+        } else if (text.equals("Wednesday")) {
+            earlyMorning.setVal(21f);
+            lateMorning.setVal(3f);
+            earlyAfternoon.setVal(5f);
+            lateAfternoon.setVal(29f);
+        } else if (text.equals("Thursday")) {
+            earlyMorning.setVal(5f);
+            lateMorning.setVal(15f);
+            earlyAfternoon.setVal(3f);
+            lateAfternoon.setVal(25f);
+        } else if (text.equals("Friday")) {
+            earlyMorning.setVal(25f);
+            lateMorning.setVal(5f);
+            earlyAfternoon.setVal(3f);
+            lateAfternoon.setVal(13f);
+        }
+
+        trendsChart.invalidate();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
